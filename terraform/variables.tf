c57918643ab77a973d4c6dd6777c58fc814e073d
Mike Harmon <mdh266@gmail.com>
2026-09-15 18:41:51 -0400

    TERRAFOM POWER

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,25 @@
 variable "project_id" {
   description = "GCP project ID"
   type        = string
+  sensitive   = true
 }
 
 variable "region" {
   description = "Region for Cloud Run"
   type        = string
   default     = "us-central1"
+  sensitive   = false
 }
 
 variable "service_name" {
   description = "Name of the Cloud Run service"
   type        = string
   default     = "agentsmcp"
+  sensitive   = false
 }
 
 variable "container_image" {
   description = "Full container image reference (including tag) – e.g. us-docker.pkg.dev/<project>/<repo>/image:tag"
   type        = string
+  sensitive   = false
 }
